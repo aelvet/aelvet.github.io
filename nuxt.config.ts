015516a153483663fe9704b6a5e3d04e99452c85
aelvet <aelvet@sina.com>
2023-06-16 10:54:40 +0800

    build: 打包

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    buildAssetsDir: '/docs/'
-  },
+  // app: {
+  //   buildAssetsDir: '/docs/'
+  // },
   devtools: { enabled: true },
+  // buildDir: 'docs'
 })
