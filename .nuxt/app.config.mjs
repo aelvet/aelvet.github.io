9ef6a7aabc350d6525e6a40620e7d645e76d92c8
aelvet <aelvet@sina.com>
2023-06-16 10:12:37 +0800

    chore: other

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/aelvet/Workspace/vue3/aelvet.github.io/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
