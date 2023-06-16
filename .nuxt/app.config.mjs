92a68efe75e277a7e8575ab6b69ee073cf490e4b
aelvet <aelvet@sina.com>
2023-06-16 11:07:13 +0800

    Delete .nuxt directory

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/aelvet/Workspace/vue3/aelvet.github.io/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
